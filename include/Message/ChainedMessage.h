38a3447b1a4d106043febdc062c1c7df3f6f3dc3
Michal Stawinski <michal.stawinski@gmail.com>
2011-05-16 22:58:42 +0200

    Added new Message class: ChainedMessage

---
+++ include/Message/ChainedMessage.h
@@ -0,0 +1,34 @@
+/*
+ * ChainedMessage.h
+ *
+ *  Created on: May 16, 2011
+ *      Author: nazgee
+ */
+
+#ifndef CHAINEDMESSAGE_H_
+#define CHAINEDMESSAGE_H_
+
+#include "Message.h"
+#include <boost/ptr_container/ptr_vector.hpp>
+
+
+namespace osock
+{
+
+class ChainedMessage: public osock::Message
+{
+private:
+	boost::ptr_vector<Message> itsLinks;
+
+public:
+	ChainedMessage();
+	~ChainedMessage();
+	void AddLink(Message* mgs2add);
+	virtual osock::data_chunk Unpack();
+	virtual void doFeed(osock::data_chunk& data);
+	void virtual doClear();
+};
+
+}
+
+#endif /* CHAINEDMESSAGE_H_ */
---
+++ src/Message/ChainedMessage.cpp
@@ -0,0 +1,72 @@
+/*
+ * ChainedMessage.cpp
+ *
+ *  Created on: May 16, 2011
+ *      Author: nazgee
+ */
+
+#include <defines.h>
+#include <Message/ChainedMessage.h>
+
+namespace osock
+{
+
+ChainedMessage::ChainedMessage()
+{
+	DBG_CONSTRUCTOR;
+}
+
+ChainedMessage::~ChainedMessage()
+{
+	DBG_DESTRUCTOR;
+}
+
+void ChainedMessage::AddLink(Message* mgs2add)
+{
+	itsLinks.push_back(mgs2add);
+}
+
+osock::data_chunk ChainedMessage::Unpack()
+{
+	osock::data_chunk ret;
+
+	for (unsigned int i = 0; i < itsLinks.size(); ++i) {
+		osock::data_chunk tmp = itsLinks[i].Unpack();
+		ret.insert(ret.end(), tmp.begin(), tmp.end());
+	}
+
+	return ret;
+}
+
+void ChainedMessage::doFeed(osock::data_chunk& data)
+{
+	// Concatenate new data to old remainings
+	itsRemains.insert(itsRemains.end(), data.begin(), data.end());
+
+	// Feed as much links as possible with new data
+	for (unsigned int i = 0; i < itsLinks.size(); ++i) {
+		bool complete = false;
+
+		if (!itsLinks[i].getIsComplete()) {
+			complete = itsLinks[i].Pack(itsRemains);
+			itsRemains = itsLinks[i].getRemains();
+		}
+
+		if (!complete) {
+			break;
+		} else {
+			// Chain is complete, when the last link is complete
+			setIsComplete((i + 1) == itsLinks.size());
+		}
+	}
+}
+
+void ChainedMessage::doClear()
+{
+	//DBG << "Clearing links" << std::endl;
+	for (unsigned int i = 0; i < itsLinks.size(); ++i) {
+		itsLinks[i].Clear();
+	}
+}
+
+}
