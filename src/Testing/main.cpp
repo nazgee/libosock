d42cfaadfba0530fccc8753dca62589690a9a5c6
Michal Stawinski <michal.stawinski@gmail.com>
2011-06-15 02:53:16 +0200

    First stub of unit test added

--- src/Testing/main.cpp
+++ src/Testing/main.cpp
@@ -5,13 +5,34 @@
  *      Author: nazgee
  */
 
-#include <iostream>
-#include <osock.h>
+#include <testutils.h>
 
-int main()
+#include <iostream>
+
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+
+int main( int ac, char **av )
 {
-	osock::HttpResponseFile response("badfile.html");
-	std::cout << "This will not happen if the file given above"
-			"does not exist" << std::endl;
-	return 0;
+  //--- Create the event manager and test controller
+  CPPUNIT_NS::TestResult controller;
+
+  //--- Add a listener that colllects test result
+  CPPUNIT_NS::TestResultCollector result;
+  controller.addListener( &result );
+
+  //--- Add a listener that print dots as test run.
+  CPPUNIT_NS::BriefTestProgressListener progress;
+  controller.addListener( &progress );
+
+  //--- Add the top suite to the test runner
+  CPPUNIT_NS::TestRunner runner;
+  runner.addTest( CPPUNIT_NS::TestFactoryRegistry::getRegistry(Utils::getSuiteNameUnit()).makeTest() );
+  runner.run( controller );
+
+  return result.wasSuccessful() ? 0 : 1;
 }
