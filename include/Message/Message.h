784849e32f466bdc1d40a86129bdebe15470515f
Michal Stawinski <michal.stawinski@gmail.com>
2011-06-24 18:04:17 +0200

    Comments rewording

--- include/Message/Message.h
+++ include/Message/Message.h
@@ -71,56 +71,72 @@ public:
 	static const std::string TAG;
 
 protected:
+	/**
+	 * @brief Marks Message as completely deserialized
+	 * @note Should be called only from doUnpack(), when message was able to
+	 * eventually deserialize istelf using provided data chunks
+	 * @param remains Remainin data, that was not used during de
+	 */
 	void ClosePacking(const data_chunk& remains);
+	/**
+	 * @brief Marks Message as not fully deserialized
+	 */
 	void ExtendPacking();
+	/**
+	 * @brief Returns xml/html represenatation of a message
+	 *
+	 * Uses getStringInfo() to generate contents of the returned std::string
+	 * @return std::string being xml/html represenatation of a message
+	 */
 	static std::string getAsTag(std::string value, std::string tag = "", std::string attr = "");
+	/**
+	 * @brief Returns std::string represenatation of a message (used for debugging)
+	 * @return std::string represenatation of a message
+	 */
 	virtual std::string getStringInfo() const;
 	/**
-	 * @brief Called to retrieve data_chunk representing message
+	 * @brief Returns data_chunk representing serialized Message
 	 *
-	 * This method is called from Unpack() method. It should return data_chunk
-	 * representing the message. This will be the data chunk sent when
-	 * Socket::Send() method will be used.
+	 * Data chunk returned by this message should represent whole Message, and
+	 * allow immediate deserialization when given to doFeed(). This will be data
+	 * chunk sent when Socket::Send() method will be used.
 	 *
-	 * @note This method is called only if message is complete (meaning
-	 * that getIsComplete() returns true), which means it is in a known state.
+	 * @note This method will NOT be called if message is in a middle of
+	 * deserialization (using doFeed())
 	 *
 	 * @return Serialized message
 	 */
 	virtual data_chunk doUnpack() const = 0;
 	/**
-	 * @brief Called to deserialize message
+	 * @brief Feeds Message with data to be used for deserialization
 	 *
-	 * This method is called from Pack() method. Message should use provided
-	 * data for deserialization. Concecutive chunks of data will be delivered
-	 * until message is not marked as complete.
-	 * @p Provided @c data_chunk can contain part of, whole, or even multiple
-	 * serialized messages. In order to make retrieval of all the messages
-	 * possible some rules should be followed during deserialization.
-	 *  When provided serialized data are incomplete, doUnpack() should store
-	 * or deserialize current data chunk, and return- it will be called again
-	 * once new data become available. When after consecutive doUnpack() data
-	 * calls Message is deserialized, doUnpack() should mark it as complete, and
-	 * save part of data_chunk that was not used during deserialization.
+	 * This method is called from Pack() whenever new data_chunk arrives.
+	 * Consecutive chunks of data will be delivered until message will not
+	 * mark itself as deserialized. Moreover, all extra data that was not used
+	 * during message deserialization should be returned. Marking message as
+	 * serialized and returning unused data is done in a single call to ClosePacking().
 	 *
-	 * @note In order to mark message as complete, called setIsMessage()
-	 * with @c true as argument.
-	 * @p In order to save part of data_chunk that was not used (is not a part
-	 * of current Message), doUnpack should assign it to itsRemains.
-	 *
-	 * @arg data Contains data that should be used to deserialize message. It
-	 * can be part of, complete, or even multiple messages.
+	 * @param data Contains data (possibly partial) that should be used
+	 * during deserialization of the Message.
+	 * @note Depending on Message and PDU size,different number of data chunks
+	 * might be needed to fully deserialize Message. Single data chunk can
+	 * contain only a part of, whole, or multiple Messages.
 	 */
 	virtual void doFeed(const data_chunk& data) = 0;
 	/**
 	 * @brief Called when internal state of the message should be cleared
 	 *
-	 * doRestartPacking() should result in restart of deserialization process-
-	 * doUnpack() should treat consecutive data_chunk as a start of a message().
+	 * After this message is called, internal serialization/deserialization
+	 * logic of the message should be reset. As a result, doFeed() should treat
+	 * next data chunk as beginning of the Message, and
+	 * doUnpack() should allow immediate deserialization.
 	 */
 	virtual void doRestartPacking() = 0;
 	/**
-	 * @brief Called from clone() method
+	 * @brief Returns clone of the Message
+	 *
+	 * It is used to allow containers cloning. Often default copy constructor
+	 * can be called here, but someteimes it has to be overriden.
 	 * @return Clone of this Message
 	 */
 	virtual Message* doClone() const = 0;
