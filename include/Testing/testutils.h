1a28c3d3d44f4c5885cbebf6fafde5a4cd7c2044
Michal Stawinski <michal.stawinski@gmail.com>
2011-06-26 12:36:48 +0200

    Added to_string() method to test::Utils

--- include/Testing/testutils.h
+++ include/Testing/testutils.h
@@ -9,6 +9,7 @@
 #define TESTUTILS_H_
 
 #include <iostream>
+#include <sstream>
 
 // XXX maybe make it a singleton, some time later?
 class Utils
@@ -17,6 +18,13 @@ private:
 	Utils() {}
 public:
 	static std::string getSuiteNameUnit() { return "Unit"; }
+	template <class T>
+	static std::string to_string (const T& t)
+	{
+		std::stringstream ss;
+		ss << t;
+		return ss.str();
+	}
 };
 
 #endif /* TESTUTILS_H_ */
